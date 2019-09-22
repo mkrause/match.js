f4c88e562626f6aca68a497873067dcfb425a030
mkrause <maikelkrause@gmail.com>
2019-09-23 01:17:13 +0200

    Update TypeScript declaration.

--- typings/match.d.ts
+++ typings/match.d.ts
@@ -1,93 +1,32 @@
 
-// TODO: { matcher, match, matchType, getSingleKey, matchSingleKey }
-
-// https://github.com/Microsoft/TypeScript/issues/10571
-
-// import { defaultCase } from '../src/symbols.js';
-
-
 declare module '@mkrause/match' {
+    const defaultCase : unique symbol;
     
-    // export const defaultKey : typeof defaultCase;
-    const defaultCase = '__@@default';
-    // const defaultCase : symbol;
+    type Discr = keyof any; // Discriminator is any possible object index (`string | number | symbol`)
+    type CaseList = object; // Case list can be any arbitrary object
     
-    type Discr = string | number | symbol; // Anything that can be used to index into an object
+    // Resolve the given case to its result type. For example:
+    // - In `{ foo: 42 }`, `foo` has result type `number`
+    // - In `{ foo: () => 42 }`, `foo` again has result type `number` (take the function return type)
+    // Note: this will also work if `C` is a union (see: "distributive conditionals" in TypeScript). Thus, this
+    // will also work for multiple cases (subset of a case list).
+    type ResolveCase<C> = C extends ((discr : Discr) => infer R) ? R : C;
     
-    /*
-    type CaseList = {
-        [key : string] : Function | unknown,
-    };
-    
-    export function matchAgainstObject<A, R>(
-            discriminator : Discr,
-            body : A,
-            cases : { [key : string] : R | ((body ?: A) => R) },
-        ) : R
-    
-    export const matcher : <S, A, R>(
-            parseSubject : (subject : S) => { discriminator : Discr, body : A }
-        )
-        => (
-            subject : S,
-            cases : { [key : string] : R | ((body ?: A) => R) },
-        )
-        => R;
-    */
-    
-    /*
-    export function match<A, K extends Discr, R, Q>(
-            discriminator : K,
-            // cases : { [key : string] : R | ((body ?: A) => R) },
-            // cases : { [key : string] : R },
-            cases : R,
-        ) : R extends { [key : K] : Q } ? R[K] : unknown;
-    */
-    
-    /*
-    export function match<D extends string, C extends { [key in D] : unknown }>(
+    // Type for the default matcher `match`.
+    // Note that in the most general case (unknown discriminator), the best we can do is to return the union of
+    // all (resolved) case types. However, if we know that the discriminator is a subtype of `keyof C`, then we
+    // can specifically return those case types. In particular, if the discriminator is a literal type, then we
+    // can return the exact type of the matched case.
+    type MatchFn = <D extends Discr, C extends CaseList>(
             discriminator : D,
             cases : C,
         )
-        : C[D];
-    */
-    
-    /*
-    export function match<D extends string, C extends object>(
-            discriminator : D,
-            cases : C,
-        )
-        : C extends { [key in D] : unknown }
-            ? C[D]
-            : never;
-    */
-    
-    // Check if the matched case is a function, if so use the return type of that function
-    type ResolveCase<C> = C extends ((...args : any[]) => infer R) ? R : C;
-    
-    // export function match<D extends string, C extends object, DEF extends typeof defaultCase>(
-    //         discriminator : D,
-    //         cases : C,
-    //     )
-    //     : D extends keyof C // Check if the discriminator exists within the case list
-    //         ? ResolveCase<C[D]>
-    //         : (
-    //             // Check if the case list has a "default" case, if so return its type
-    //             DEF extends keyof C ? ResolveCase<C[DEF]> : never
-    //         );
-    
-    export const match : {
-        <D extends string, C extends object, DEF extends typeof defaultCase>(
-            discriminator : D,
-            cases : C,
-        )
-        : D extends keyof C // Check if the discriminator exists within the case list
+        => D extends keyof C // Check if the discriminator exists within the case list
             ? ResolveCase<C[D]>
-            : (
-                // Check if the case list has a "default" case, if so return its type
-                DEF extends keyof C ? ResolveCase<C[DEF]> : never
-            );
-        default : typeof defaultCase;
+            : ResolveCase<C[keyof C]>; // Return the union of all possible (resolved) return values
+    
+    export const match : MatchFn & {
+        default : typeof defaultCase,
     };
     
     export default match;
--- typings/test.ts
+++ typings/test.ts
@@ -1,8 +1,9 @@
+///<reference lib="es2015"/>
 ///<reference path="./match.d.ts"/>
 
 // Test module to test TypeScript declaration file.
 // Usage:
-//   $ tsc --noEmit --strict --esModuleInterop --lib es2015 typings/test.ts
+//   $ tsc --noEmit --strict --esModuleInterop typings/test.ts
 // See: https://stackoverflow.com/questions/49296151/how-to-write-tests-for-typescript-typing-definition
 
 
@@ -23,33 +24,41 @@
 import match from '@mkrause/match';
 
 
-// Expected error: Type 'number' is not assignable to type '"test"'.
-const test1 : 'test' = match('foo', {
+// Expected error: Type 'number' is not assignable to type '"test1"'.
+const test1 : 'test1' = match('foo', {
     foo: 42,
     bar: 'hello',
 });
 
-// Expected error: Type 'number' is not assignable to type '"test"'.
-const test2 : 'test' = match('foo', {
+// Expected error: Type 'number' is not assignable to type '"test2"'.
+const test2 : 'test2' = match('foo', {
     foo: (tag : string) : number => 42,
     bar: 'hello',
 });
 
-// Expected error: Type 'boolean' is not assignable to type '"test"'.
-const test3 : 'test' = match('nonexistent', {
+// Expected error: Type 'string | number | boolean' is not assignable to type '"test3"'.
+const test3 : 'test3' = match('nonexistent', {
     [match.default]: true,
     foo: (tag : string) : number => 42,
     bar: 'hello',
 });
 
-// Expected error: Type 'boolean' is not assignable to type '"test"'.
-const test4 : 'test' = match('nonexistent', {
+// Expected error: Type 'string | number | boolean' is not assignable to type '"test4"'.
+const test4 : 'test4' = match('nonexistent', {
     [match.default]: () : boolean => true,
     foo: (tag : string) : number => 42,
     bar: 'hello',
 });
 
-// Expected error: none (result should be type `never`, which is assignable to any type)
-const test5 : never = match('nonexistent', {
+// Expected error: Type 'number' is not assignable to type '"test5"'.
+const test5 : 'test5' = match('nonexistent', {
     foo: 42,
+});
+
+
+// Expected error: Type 'string | number' is not assignable to type '"test6"'.
+const d : string = 'random' + Math.random();
+const test6 : 'test6' = match(d, {
+    foo: (tag : string) : number => 42,
+    bar: 'hello',
 });
